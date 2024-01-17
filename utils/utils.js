88e59999d0a03f05e8cefda2794d926856565e16
livdawson <olivia.dawson@btinternet.com>
2024-01-17 10:19:26 +0000

    set up delete request for /api/comments/:comment_id endpoint

--- utils/utils.js
+++ utils/utils.js
@@ -5,8 +5,16 @@ exports.checkArticleIDexists = (article_id) => {
     SELECT * FROM articles 
     WHERE article_id = $1`, [article_id])
     .then(({ rows }) => {
-        if (rows.length === 0) {
-            return Promise.reject({status: 404, msg: 'article_id not found'})
-        }
+        if (rows.length === 0) return Promise.reject({status: 404, msg: 'article_id not found'})
+    })
+};
+
+exports.checkCommentIDexists = (comment_id) => {
+    return db.query(`
+    SELECT * FROM comments
+    WHERE comment_id = $1
+    `, [comment_id])
+    .then(( { rows }) => {
+        if(rows.length === 0) return Promise.reject({status: 404, msg: 'comment_id not found'})
     })
 }
