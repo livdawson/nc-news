c19de2d9170fbb8cc862b814642b4d2f7469bf20
livdawson <olivia.dawson@btinternet.com>
2024-01-18 12:26:23 +0000

    refactor to enable hosting

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,10 @@
 const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'production';
+const ENV = process.env.NODE_ENV || 'development';
+
+
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
 
 const config = {};
 
@@ -8,12 +13,9 @@ if (ENV === 'production') {
   config.max = 2
 }
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
+
 
 module.exports = new Pool(config);
