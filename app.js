098221310632e1f63274057a984871c474071fc5
livdawson <olivia.dawson@btinternet.com>
2024-01-19 11:53:33 +0000

    refactor to encorporate express routers

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
 const express = require("express");
-const { getTopics } = require("./controllers/topics.controllers");
-const { handleCustomErrors, handleSqlErrors, internalServerErrors } = require("./errors");
-const { getAllAvailableEndpoints } = require("./controllers/endpoints.controllers");
-const { getArticleByArticleID, getArticles, patchArticleVotes } = require("./controllers/articles.controllers");
-const { getCommentsByArticleID, postNewComment, deleteComment } = require("./controllers/comments.controllers");
-const { getUsers } = require("./controllers/users.controllers");
 const app = express();
+const apiRouter = require("./routes/api-router");
+const { handleCustomErrors, handleSqlErrors, internalServerErrors } = require("./errors");
 
 app.use(express.json())
 
-app.get('/api', getAllAvailableEndpoints)
-
-app.get('/api/topics', getTopics);
-
-app.get('/api/articles', getArticles)
-
-app.get('/api/articles/:article_id', getArticleByArticleID);
-
-app.get('/api/articles/:article_id/comments', getCommentsByArticleID);
-
-app.post('/api/articles/:article_id/comments', postNewComment);
-
-app.patch('/api/articles/:article_id', patchArticleVotes);
-
-app.delete('/api/comments/:comment_id', deleteComment);
-
-app.get('/api/users', getUsers);
+app.use('/api', apiRouter)
 
 app.use(handleCustomErrors)
 
